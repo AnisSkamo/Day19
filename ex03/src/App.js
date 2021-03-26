4a8e7c1cf10e724a33542ce63879b84d64ccea41
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-26 22:41:39 +0100

    Add files via upload

--- ex03/src/App.js
+++ ex03/src/App.js
@@ -1,1 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+ReactDOM.render(<App />, document.getElementById('root'));
 
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, Padawans!</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
