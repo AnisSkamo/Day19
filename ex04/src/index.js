cd5e750399ec2373a07b9a09a7eb5e2d9e695496
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-26 22:57:53 +0100

    Add files via upload

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,1 +1,17 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
+class App extends Component { 
+  render() {
+    return (
+      <div ClassName="App">
+        <h1>Hello, React"</h1>
+      </div>
+    )
+  }
+}
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reactDom.render(<App />, document.getElementById('root'));
